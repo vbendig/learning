d4ae8d3d9453e2fac0c09bb93b1c5bc243b8fe02
Victor Bendig <vbendig@gmail.com>
2021-08-17 14:43:30 -0600

    removing terraform examples

--- terraform/amount/shared/partners-sandbox/ads/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  ads_validating_datasets_bucket_name = "amount-${var.partner}-${var.env}-ads-validating-datasets"
-  ads_validated_datasets_bucket_name = "amount-shared-${var.env}-ads-validated-datasets"
-}
