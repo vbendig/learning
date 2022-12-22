f7910e791eb6b285bd048bbdc500ba7c150181a2
Victor Bendig <vbendig@gmail.com>
2022-12-22 17:22:15 -0600

    Adding changes for java exercises

--- java/exercises/src/test/java/CountingDuplicateCharactersTest.java
+++ java/exercises/src/test/java/CountingDuplicateCharactersTest.java
@@ -1,4 +1,48 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CountingDuplicateCharactersTest {
-  
+
+    @Test
+    void countDuplicateChars() {
+        int result = CountingDuplicateCharacters.countDuplicateChars("abccd");
+        assertSame(1, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateChars("abccdd");
+        assertSame(2, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateChars("abcd");
+        assertSame(0, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateChars("aabcd");
+        assertSame(1, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateChars("abbbbcd");
+        assertSame(1, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateCharsRecursive("aaaaaabbbbcccccccddddd");
+        assertSame(4, result, "Should be same");
+    }
+
+    @Test
+    void countDuplicateCharsRecursive() {
+        int result = CountingDuplicateCharacters.countDuplicateCharsRecursive("abccd");
+        assertSame(1, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateCharsRecursive("abccdd");
+        assertSame(2, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateCharsRecursive("abcd");
+        assertSame(0, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateCharsRecursive("aabcd");
+        assertSame(1, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateCharsRecursive("abbbbcd");
+        assertSame(1, result, "Should be same");
+
+        result = CountingDuplicateCharacters.countDuplicateCharsRecursive("aaaaaabbbbcccccccddddd");
+        assertSame(4, result, "Should be same");
+    }
 }
---
+++ spring/springweb/SpringWebDemo/src/test/java/com/springweb/SpringWebDemo/SpringWebDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springweb.SpringWebDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDemoTests {
+
+	@Test
+	void contextLoads() {
+		assert(true);
+	}
+
+}
